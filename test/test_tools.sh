5565a34fd8d1b375667b37efd5a9af7640a4b5ff
MaximeVdB <maxime.cp.vandenbossche@gmail.com>
2020-06-22 09:57:26 +0200

    MAINT: rename tools to vsc-prc-<command>

--- test/test_tools.sh
+++ test/test_tools.sh
@@ -11,39 +11,39 @@ icd $tmpdir
 irods_path="~/"$tmpdir
 
 # Copy content to iRODS
-echo "TEST: bulk-iput"
-bulk-iput -r ../test/data* -d $irods_path --verbose
+echo "TEST: vsc-prc-iput"
+vsc-prc-iput -r ../test/data* -d $irods_path --verbose
 echo "iRODS tmpdir content:"
 ils -r .
 
 # Fetch content to local directory
-echo "TEST: bulk-iget"
+echo "TEST: vsc-prc-iget"
 local_tmpdir=`mktemp -d`
 echo "Local tmpdir path: "$local_tmpdir
-bulk-iget -r $irods_path"/data/molecules/" -d $local_tmpdir --verbose
+vsc-prc-iget -r $irods_path"/data/molecules/" -d $local_tmpdir --verbose
 echo "Local tmpdir content: "
 find $local_tmpdir
 echo "Removing local tmpdir "$local_tmpdir
 rm -r $local_tmpdir
 
 # Add metadata to certain files
-echo "TEST: bulk-imeta (add)"
-bulk-imeta $irods_path"/data/molec*/c*.xyz" --object_avu=Kind,organic \
+echo "TEST: vsc-prc-imeta (add)"
+vsc-prc-imeta $irods_path"/data/molec*/c*.xyz" --object_avu=Kind,organic \
            --action=add --verbose
 
-# Try to find these files using search-find with an AVU filter
-echo "TEST: search-find"
-search-find $irods_path"/data/molecules/" --object_avu="=,kind;like,%org%" \
+# Try to find these files using vsc-prc-find with an AVU filter
+echo "TEST: vsc-prc-find"
+vsc-prc-find $irods_path"/data/molecules/" --object_avu="=,kind;like,%org%" \
             --types=f --debug
 
 # Remove that same metadata
-echo "TEST: bulk-imeta (remove)"
-bulk-imeta $irods_path"/data/molec*/c*.xyz" --object_avu=Kind,organic \
+echo "TEST: vsc-prc-imeta (remove)"
+vsc-prc-imeta $irods_path"/data/molec*/c*.xyz" --object_avu=Kind,organic \
            --action=remove --verbose
 
 # Remove all the uploaded content
-echo "TEST: bulk-irm"
-bulk-irm -r $irods_path"/data/" --verbose
+echo "TEST: vsc-prc-irm"
+vsc-prc-irm -r $irods_path"/data/" --verbose
 
 # Remove the now-empty test collection
 echo "Removing test collection "$tmpdir
